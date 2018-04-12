afd419b0b79df98776c4b3b3db82d37ede58bc05
a497579916 <497579916@qq.com>
2018-04-12 23:43:38 +0800

    1

---
+++ jl/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+     
+   
+      $("#donghua").animate({
+      
+      height:'120px',
+      width:'120px'
+      });
+
+      $("#donghua2").addClass("animated rubberBand");
+      $(".donghua3").addClass("animated rubberBand");
+
+
+      $(".donghua4").addClass("animated bouncelnDown");
+
+
+      
+      
+     
+
+	 
+
+  });
+ 
+  
+
+
+
+  window.onload=function(){
+    $(".flash").remove()}
+
+
+
+
